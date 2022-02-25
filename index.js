96a89e63c1f0c74205c6095737f3aab400980994
Harry <88223850+Harry-0630@users.noreply.github.com>
2022-02-25 10:37:15 +0530

    index file main bot file.

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv')
+const { Client, Util, Collection, MessageEmbed, Structures } = require("discord.js");
+const keepAlive = require('./server.js')
+keepAlive()
+async function errorEmbed(text, message) {
+  const newembed = new Discord.MessageEmbed()
+    .setColor("#FF7676")
+    .setDescription(`**❌ | ${text} **`)
+  return message.channel.send(newembed);
+}
+const Discord = require('discord.js');
+const client = new Client({
+  disableEveryone: true
+})
+const axios = require("axios")
+
+
+//=============================================
+const channel_id = "945957450861920256"
+//=============================================
+
+
+client.on('message', async (message) => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  try {
+    if (message.channel.id != channel_id) return
+    let res = await axios.get(`http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=1&msg=${encodeURIComponent(message.content)}`);
+    message.reply(res.data.cnt);
+  } catch {
+    errorEmbed(`Bot error, please try again!`, message)
+  }
+})
+
+client.on('ready', async () => {
+  client.user.setActivity("Coding with Harry and hvaing fun 😄", {
+    type: "PLAYING",
+  });
+  console.clear()
+  console.log(`${client.user.tag} is online!`)
+  client.user.setStatus("online");
+})
+
+client.login(process.env.TOKEN);
